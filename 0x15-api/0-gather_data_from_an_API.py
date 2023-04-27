02b7a68af5e09139aa6be65befdfc59fc117e404
Myles2005 <cyrile450@gmail.com>
2023-04-26 17:51:36 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Returns to-do list information for a given employee ID."""
 import sys
-import request
+import requests
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
