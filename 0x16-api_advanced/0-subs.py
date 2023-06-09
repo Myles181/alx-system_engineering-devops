b39ad2c8abe7db3e4eaf432a2ea9e00730375d3d
Myles2005 <cyrile450@gmail.com>
2023-06-08 20:00:59 -0700

    0-sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,8 +9,7 @@ def number_of_subscribers(subreddit):
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    res = requests.get(url, headers=headers#allow_redirects=False
-            )
+    res = requests.get(url, headers=headers)
     if res.status_code == 404:
         return 0
     results = res.json().get("data")
